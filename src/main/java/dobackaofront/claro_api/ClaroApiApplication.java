e1299e9712a16058167051be5498bb0d8cf1ef7f
Olival Paulino <olivalpaulinocc@gmail.com>
2024-07-25 13:21:25 -0300

    api claro primeiro commit

---
+++ src/main/java/dobackaofront/claro_api/ClaroApiApplication.java
@@ -0,0 +1,13 @@
+package dobackaofront.claro_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaroApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaroApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dobackaofront/claro_api/ClaroApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dobackaofront.claro_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaroApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
